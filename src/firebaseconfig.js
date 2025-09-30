1cfcec01b9991b92696fa05e89072e53fd7580a4
shobhitkothiyal2-lang <shobhitkothiyal2@gmail.com>
2025-09-30 13:10:57 +0530

    firebasecofig

---
+++ src/firebaseconfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDsKHXLG4zkYjU5ksVoZlYR-krsMfDsKUk",
+  authDomain: "blogweb-cf859.firebaseapp.com",
+  projectId: "blogweb-cf859",
+  storageBucket: "blogweb-cf859.firebasestorage.app",
+  messagingSenderId: "902320249703",
+  appId: "1:902320249703:web:69495f65e93776252f2dc2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
